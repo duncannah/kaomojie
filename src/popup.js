d4761ec5019a3846be605a94c9c36bb03f923ae3
duncannah <38371914+duncannah@users.noreply.github.com>
2025-03-16 22:27:33 +0100

    feat: refactor App component

--- src/popup.js
+++ src/popup.js
@@ -1,112 +1,136 @@
 document.documentElement.lang = chrome.i18n.getMessage("lang");
 
+const CategoryList = ({ categories, curCat, onChangeCat }) => (
+	<div className="categories">
+		{categories.map((cat) => (
+			<li
+				key={cat}
+				className={curCat === cat ? "active" : ""}
+				onClick={() => onChangeCat(cat)}
+			>
+				{chrome.i18n.getMessage(`category__${cat}`) || cat}
+			</li>
+		))}
+	</div>
+);
+
+const SubCategoryList = ({ subCategories, curSub, onChangeSub }) => (
+	<div className="sub-categories">
+		{subCategories.map((subCat) => (
+			<li
+				key={subCat}
+				className={curSub === subCat ? "active" : ""}
+				onClick={() => onChangeSub(subCat)}
+			>
+				{chrome.i18n.getMessage(`subCategory__${subCat}`) || subCat}
+			</li>
+		))}
+	</div>
+);
+
+const KaomojiList = ({ kaomojis }) => (
+	<div className="scroll-area">
+		<div className="kaomojis">
+			{kaomojis.map((kaomoji, i) => (
+				<li key={i} onMouseDown={() => navigator.clipboard.writeText(kaomoji)}>
+					{kaomoji}
+				</li>
+			))}
+		</div>
+	</div>
+);
+
+const InfoPanel = ({ showInfo, toggleInfo }) => (
+	<div className={"info" + (showInfo ? " show" : "")}>
+		<div className="info-close" onClick={toggleInfo}>
+			✕
+		</div>
+		<div className="info-content">
+			<div className="info-title">kaomojie</div>
+			<div className="info-version">
+				{chrome.i18n.getMessage(
+					"version",
+					chrome.runtime ? chrome.runtime.getManifest().version : "..."
+				)}
+			</div>
+			<p>
+				{chrome.i18n.getMessage("sourceCodeDisclaimer")}
+				<br />
+				<a
+					href="https://github.com/duncannah/kaomojie"
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					https://github.com/duncannah/kaomojie
+				</a>
+			</p>
+		</div>
+	</div>
+);
+
 class App extends preact.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			curCat: Object.keys(KAOMOJIS)[0],
 			curSub: Object.keys(KAOMOJIS[Object.keys(KAOMOJIS)[0]])[0],
-
 			showInfo: false,
 		};
 	}
 
-	componentDidMount = () => {
+	componentDidMount() {
 		chrome.storage.local.get(["curCat", "curSub"], (res) => {
 			if (res.curCat) this.setState({ curCat: res.curCat, curSub: res.curSub });
 		});
+	}
+
+	componentDidUpdate(_, prevState) {
+		if (prevState.curSub !== this.state.curSub) {
+			document.getElementsByClassName("scroll-area")[0].scrollTo(0, 0);
+		}
+	}
+
+	changeCat = (cat) => {
+		const newSub = Object.keys(KAOMOJIS[cat])[0];
+		this.setState({ curCat: cat, curSub: newSub });
+		chrome.storage.local.set({ curCat: cat, curSub: newSub });
 	};
 
-	componentDidUpdate = () => {
-		document.getElementsByClassName("scroll-area")[0].scrollTo(0, 0);
+	changeSub = (subCat) => {
+		this.setState({ curSub: subCat });
+		chrome.storage.local.set({ curCat: this.state.curCat, curSub: subCat });
 	};
 
-	_changeCat = (c) => {
-		this.setState({ curCat: c, curSub: Object.keys(KAOMOJIS[c])[0] });
-
-		chrome.storage.local.set({ curCat: c, curSub: Object.keys(KAOMOJIS[c])[0] });
-	};
-
-	_changeSub = (c) => {
-		this.setState({ curSub: c });
-
-		chrome.storage.local.set({ curCat: this.state.curCat, curSub: c });
-	};
-
-	_toggleInfo = () => {
+	toggleInfo = () => {
 		this.setState({ showInfo: !this.state.showInfo });
 	};
 
-	render = () => {
+	render() {
+		const { curCat, curSub, showInfo } = this.state;
+		const categories = Object.keys(KAOMOJIS);
+		const subCategories = Object.keys(KAOMOJIS[curCat]);
+		const kaomojis = KAOMOJIS[curCat][curSub];
+
 		return (
 			<>
-				<div className="info-btn" onClick={this._toggleInfo}>
+				<div className="info-btn" onClick={this.toggleInfo}>
 					kaomojie
 				</div>
-				<div className="categories">
-					{Object.keys(KAOMOJIS).map((c) => (
-						<li
-							key={c}
-							className={this.state.curCat === c ? "active" : ""}
-							onClick={() => this._changeCat(c)}
-						>
-							{chrome.i18n.getMessage(`category__${c}`) || c}
-						</li>
-					))}
-				</div>
-				<div className="sub-categories">
-					{Object.keys(KAOMOJIS[this.state.curCat]).map((c) => (
-						<li
-							key={c}
-							className={this.state.curSub === c ? "active" : ""}
-							onClick={() => this._changeSub(c)}
-						>
-							{chrome.i18n.getMessage(`subCategory__${c}`) || c}
-						</li>
-					))}
-				</div>
-				<div className="scroll-area">
-					<div className="kaomojis">
-						{KAOMOJIS[this.state.curCat][this.state.curSub].map((c, i) => (
-							<li
-								key={this.state.curCat + this.state.curSub + i}
-								onMouseDown={() => navigator.clipboard.writeText(c)}
-							>
-								{c}
-							</li>
-						))}
-					</div>
-				</div>
-
-				<div className={"info" + (this.state.showInfo ? " show" : "")}>
-					<div className="info-close" onClick={this._toggleInfo}>
-						✕
-					</div>
-					<div className="info-content">
-						<div className="info-title">kaomojie</div>
-						<div className="info-version">
-							{chrome.i18n.getMessage(
-								"version",
-								chrome.runtime ? chrome.runtime.getManifest().version : "..."
-							)}
-						</div>
-						<p>
-							{chrome.i18n.getMessage("sourceCodeDisclaimer")}
-							<br />
-							<a
-								href="https://github.com/duncannah/kaomojie"
-								rel="noopener noreferrer"
-								target="_blank"
-							>
-								https://github.com/duncannah/kaomojie
-							</a>
-						</p>
-					</div>
-				</div>
+				<CategoryList
+					categories={categories}
+					curCat={curCat}
+					onChangeCat={this.changeCat}
+				/>
+				<SubCategoryList
+					subCategories={subCategories}
+					curSub={curSub}
+					onChangeSub={this.changeSub}
+				/>
+				<KaomojiList kaomojis={kaomojis} />
+				<InfoPanel showInfo={showInfo} toggleInfo={this.toggleInfo} />
 			</>
 		);
-	};
+	}
 }
 
 preact.render(<App />, document.getElementById("app"));
